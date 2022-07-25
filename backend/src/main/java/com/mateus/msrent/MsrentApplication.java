aef114a733c2311bc14f280ddfb54298b867023e
MateusLBSantos <mateuslucas.ita@hotmail.com>
2022-07-25 16:29:27 -0300

    Repository started

---
+++ backend/src/main/java/com/mateus/msrent/MsrentApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.msrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mateus/msrent/MsrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.msrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
