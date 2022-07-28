8d56ed4f9ba28189c2718a4e9cada2b7f0855d37
MateusLBSantos <mateuslucas.ita@hotmail.com>
2022-07-28 10:54:30 -0300

    Insert, update, delete

--- backend/src/main/java/com/mateus/msrent/repositories/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/mateus/msrent/repositories/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.mateus.msrent.services.exceptions.DataBaseException;
 import com.mateus.msrent.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -28,4 +29,19 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(status).body(err);
 	}
-}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBaseBadRequest(DataBaseException e, HttpServletRequest request) {
+
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
+	}
--- backend/src/main/java/com/mateus/msrent/services/ClientService.java
+++ backend/src/main/java/com/mateus/msrent/services/ClientService.java
@@ -4,7 +4,11 @@ package com.mateus.msrent.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mateus.msrent.dto.ClientDTO;
 import com.mateus.msrent.entities.Client;
 import com.mateus.msrent.repositories.ClientRepository;
+import com.mateus.msrent.services.exceptions.DataBaseException;
 import com.mateus.msrent.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -35,6 +40,49 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não existente"));
 		
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copiaDtoParaEntity(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não existe " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Integridade violada");
+		}
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			copiaDtoParaEntity(dto, entity);
+			entity = repository.save(entity);
+			
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id não encontrado " + id);
+		}
+	}
+	
+	private void copiaDtoParaEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 
 }
